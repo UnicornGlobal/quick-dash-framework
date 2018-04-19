3386a296da6664dccf0c93f503dde798b2b47fec
Bamidele Daniel <greatbolutife@gmail.com>
2018-04-19 10:31:37 +0100

    redesign VerifyAccountCard component

--- src/components/Cards/VerifyAccountCard.vue
+++ src/components/Cards/VerifyAccountCard.vue
@@ -1,30 +1,44 @@
 <template>
-  <div class="action-required-card">
-    <div class="mobile-layout">
-      <div class="action-description">
-        <h3 class="title">Email requires verification</h3>
-        <p class="description">
-          Please confirm your email address <strong>{{ user.email }}</strong> from the email we sent you when you signed up.
-        </p>
+  <div class="cont">
+    <div class="action-required-card">
+      <div class="mobile-layout">
+        <div>
+          <info class="info-icon"></info>
+        </div>
+        <div class="action-description">
+          <h3 class="title">Email requires verification</h3>
+          <p class="description">
+            Please confirm your email address <strong>{{ user.email }}</strong> from the email we sent you when you signed up.
+          </p>
+        </div>
       </div>
-    </div>
-    <div>
-      <div v-if="this.$store.getters.resentVerification"></div>
-      <button v-else @click="resendMail" class="action-required-link">Resend Email</button>
+      <div>
+        <div v-if="this.$store.getters.resentVerification"></div>
+        <button v-else @click="resendMail" class="action-required-link">Resend Email</button>
+      </div>
     </div>
   </div>
 </template>
 
 <style lang="scss" scoped>
+  .cont {
+    background-color: $white;
+    padding: 15px;
+  }
+  .info-icon {
+    height: 50px;
+    width: 50px;
+    margin-right: 20px;
+  }
   .action-required-card {
     display: flex;
     flex-direction: row;
-    border: 2px solid $success;
-    background-color: lighten($success, 35%);
-    margin: 1em;
+    border: 1px solid $blue-2;
+    background-color: $blue-light;
     padding: 1em;
     align-items: center;
     justify-content: space-between;
+    border-radius: 3px;
     @media(min-width: 1400px) {
       justify-content: center;
     }
@@ -65,15 +79,21 @@
       }
 
       .title {
-        font-size: 16px;
+        font-size: 18px;
         margin: 0;
         font-weight: bold;
+        color: $blue-1;
       }
 
       p {
         height: 100%;
-        font-size: 14px;
+        font-size: 13px;
         flex: 1;
+        margin: 5px 0px;
+
+        strong {
+          color: $primary
+        }
       }
     }
 
@@ -84,30 +104,39 @@
       white-space: nowrap;
       vertical-align: middle;
       user-select: none;
-      border: 2px solid;
+      border: 1px solid;
       padding: .375rem 1rem;
       margin-left: 1rem;
-      font-size: 1rem;
+      font-size: .8rem;
       line-height: 1.5;
       border-radius: 0;
       transition: background-color .15s ease-in-out, border-color .15s ease-in-out, box-shadow .15s ease-in-out;
-      color: $primary-text;
-      background-color: $primary-light;
+      color: $white;
+      background-color: $blue-2;
       background-image: none;
-      border-color: lighten($primary, 10%);
+      border-color: $blue-1;
+      text-transform: uppercase;
       text-decoration: none;
+      border-radius: 3px;
+      cursor: pointer;
+
+      &:hover {
+        background-color: darken($blue-2, 10)
+      }
     }
   }
 </style>
 
 <script>
   import Card from '@/components/Cards/Card.vue'
+  import Info from '@/components/Icons/info.svg'
   import { resendVerification } from '@/api/user'
 
   export default {
     name: 'action-required-card',
     components: {
-      Card
+      Card,
+      Info
     },
     props: {
       user: {
