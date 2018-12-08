bdb0cb5fb5736494976c734e564300506b213fa5
Darryn Ten <darryn.ten@gmail.com>
2018-12-08 12:07:03 +0200

    Move icons to assets folder

--- src/components/Cards/VerifyAccountCard.vue
+++ src/components/Cards/VerifyAccountCard.vue
@@ -86,7 +86,7 @@
 
 <script>
   import Card from '@/components/Cards/Card.vue'
-  import Info from '@/components/Icons/info.svg'
+  import Info from '@/assets/icons/info.svg'
   import { resendVerification } from '@/api/user'
 
   export default {
