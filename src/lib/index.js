8f4ef67ca0164c0255954519551dc12dc5fe751f
Darryn Ten <darryn.ten@gmail.com>
2018-12-08 11:26:58 +0200

    Load custom libraries from host applications

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,13 +1,40 @@
-const req = require.context('.', true, /\.js$/)
+/**
+ * Base framework level libraries are represented in `@/lib`
+ *
+ * Host application level libraries are represented in `~/lib`
+ */
+const local = require.context('@/lib', true, /\.js$/)
+
+// TODO there's a much better way of doing this
+let host = false
+try {
+  host = require.context('~/lib', true, /\.js$/)
+} catch (e) {
+  console.error('Application level `lib` folder is missing')
+}
 
 export default {
   load: () => {
-    req.keys().forEach(function(key) {
+    local.keys().forEach(function(key) {
       if (key === './index.js') {
         return
       }
-      console.log('loading', key)
-      req(key)
+
+      console.log('Loading framework level library config: ', key)
+      local(key)
     })
+
+    if (host) {
+      console.log('Host application has custom `lib` folder')
+      host.keys().forEach(function(key) {
+        if (key === './index.js') {
+          console.error('Do not place `index.js` files in your `/lib` directory')
+          return
+        }
+
+        console.log('Loading application level library config: ', key)
+        host(key)
+      })
+    }
   }
 }
