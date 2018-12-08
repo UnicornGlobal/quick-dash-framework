27c9eb4ed5cd8956d81b7a03767262e62fcc90da
Darryn Ten <darryn.ten@gmail.com>
2018-12-08 11:02:42 +0200

    Load custom stores from client applications

--- src/store/index.js
+++ src/store/index.js
@@ -1,22 +1,76 @@
 import Vue from 'vue'
 import VueX from 'vuex'
 
-import user from './user'
-import common from './common'
+import user from '@/store/user'
+import common from '@/store/common'
 
 Vue.use(VueX)
 
-export default new VueX.Store({
-  state: {
-    ...user.state,
-    ...common.state
-  },
-  mutations: {
-    ...user.mutations,
-    ...common.mutations
-  },
-  getters: {
-    ...user.getters,
-    ...common.getters
+/**
+ * Base framework level store
+ *
+ * We load these explicitly, we do not autoload all files found in
+ * the framework level store folder.
+ */
+let state = {
+  ...user.state,
+  ...common.state
+}
+
+let mutations = {
+  ...user.mutations,
+  ...common.mutations
+}
+
+let getters = {
+  ...user.getters,
+  ...common.getters
+}
+
+/**
+ * Custom store
+ *
+ * This is loaded from the host application via directory naming
+ * convention.
+ *
+ * The convention is to load anything not called `index.js` from
+ * the `src/store` folder in the host application after the built
+ * in framework level requirements have been loaded.
+ *
+ * ~/ represents the host application `src` directory
+ * @/ represents this frameworks `src` directory
+ */
+const req = require.context('~/store', true, /\.js$/)
+req.keys().forEach(function(key) {
+  /**
+   * Do not load any index.js files that are placed in the host
+   * applications `store` folder
+   */
+  if (key === './index.js') {
+    console.error('Do not place `index.js` files in your `/store` directory')
+    return
   }
+
+  console.log('Found custom store: ', key)
+
+  state = {
+    ...state,
+    ...req.state
+  }
+
+  mutations = {
+    ...mutations,
+    ...req.mutations
+  }
+
+  getters = {
+    ...getters,
+    ...req.getters
+  }
+})
+
+export default new VueX.Store({
+  state,
+  mutations,
+  getters
 })
