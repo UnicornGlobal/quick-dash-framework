ccdcfa13df8f063a9a8181df3645c8ee0aecc6f2
Darryn Ten <darryn.ten@gmail.com>
2018-12-09 15:54:25 +0200

    Reworked forgot password page

---
+++ src/i18n/en/password-reset.js
@@ -0,0 +1,6 @@
+export default {
+  header: 'Forgot Password?',
+  details: 'Enter your email address to receive instruction on how to reset your password.',
+  success: 'Check your inbox for more info',
+  button: 'SEND'
+}
