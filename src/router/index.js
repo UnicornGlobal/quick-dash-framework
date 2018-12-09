78678deec75d9ca6d02e69a74c0772aac3bc2102
Darryn Ten <darryn.ten@gmail.com>
2018-12-09 15:12:11 +0200

    Load custom routes from host application

--- src/router/index.js
+++ src/router/index.js
@@ -9,18 +9,67 @@ import appRoute from '@/router/base'
 
 import { userHasRole } from '@/auth'
 
-let router = new Router({
-  mode: 'history',
-  routes: [...authRoutes]
-})
+/**
+ * Custom routes
+ *
+ * This is loaded from the host application via directory naming
+ * convention.
+ *
+ * The convention is to load anything not called `index.js` from
+ * the `src/routes` folder in the host application after the built
+ * in framework level requirements have been loaded.
+ *
+ * ~/ represents the host application `src` directory
+ * @/ represents this frameworks `src` directory
+ */
+async function getCustomRoutes() {
+  let custom = false
+  let result = []
+  try {
+    custom = require.context('~/router', true, /\.js$/)
+    custom.keys().forEach(function(key) {
+      /**
+       * Do not load any index.js files that are placed in the host
+       * applications `store` folder
+       */
+      if (key === './index.js') {
+        console.warn('Do not place `index.js` files in your `/router` directory, they are ignored.')
+        return
+      }
+
+      if (key === './auth.js') {
+        console.warn('Do not place `auth.js` files in your `/router` directory, they are ignored.')
+        return
+      }
+
+      if (key === './base.js') {
+        console.warn('Do not place `base.js` files in your `/router` directory, they are ignored.')
+        return
+      }
+
+      console.log('Found custom routes: ', key)
+      const value = custom(key).default
+      console.log('val', value)
+      result = [...result, ...value]
+    })
+    console.log(result)
+    return result
+  } catch (e) {
+    console.error('Application level `router` folder is missing')
+  }
+}
 
 export async function loadRoutes(user) {
-  let routes = userRoutes
+  let customRoutes = await getCustomRoutes()
+  let routes = [...userRoutes, ...customRoutes]
 
   if (userHasRole(user, 'ADMIN')) {
     routes = [...routes, ...adminRoutes]
   }
 
+  // Unique
+  routes = await [...new Set(routes)]
+  console.log('f', routes)
   appRoute.children = routes
 
   store.commit('setRoutes', [appRoute])
@@ -33,6 +82,11 @@ export function reloadRouter() {
     routes: [...authRoutes]
   })
 }
+
+let router = new Router({
+  mode: 'history',
+  routes: [...authRoutes]
+})
 
 router.afterEach(function (to, from) {
   store.commit('showSideBar', false)
