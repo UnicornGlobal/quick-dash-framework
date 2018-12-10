1cc614c3a6859e62529795399a7875e59f823e21
Igor <igorsergiichuk@gmail.com>
2018-12-10 18:58:15 +0200

    Users component fix

--- src/api/admin/users.js
+++ src/api/admin/users.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
+import store from '@/store'
 
 export function loadAllUsers() {
-  return Vue.axios.get('/api/admin/users/all').then((response) => {
+  return Vue.axios.get('/api/admin/users').then((response) => {
+    store.commit('setAllUsers', response.data.users)
     return response.data
   })
 }
