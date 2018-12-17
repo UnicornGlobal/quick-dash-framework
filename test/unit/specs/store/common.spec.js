96343ee2ed82cca48d56d7e3930fb3b21fcef581
Darryn Ten <darryn.ten@gmail.com>
2018-12-17 16:04:42 +0200

    Store tests

--- test/unit/specs/store/common.spec.js
+++ test/unit/specs/store/common.spec.js
@@ -1,39 +1,39 @@
 import store from '@/store'
 
-describe('store/app', () => {
+describe('store/common.js', () => {
   it('is an object', () => {
     expect(store).to.be.an('Object')
   })
 
   it('sets and gets loading state', () => {
-    store.commit('app/loading', true)
-    expect(store.getters['app/loading']).to.equal(true)
+    store.commit('setLoading', true)
+    expect(store.getters.loading).to.equal(true)
 
-    store.commit('app/loading', false)
-    expect(store.getters['app/loading']).to.equal(false)
+    store.commit('setLoading', false)
+    expect(store.getters.loading).to.equal(false)
   })
 
   it('sets and gets sidebar state', () => {
-    store.commit('app/sidebar/open', true)
-    expect(store.getters['app/sidebar/open']).to.equal(true)
+    store.commit('showSideBar', true)
+    expect(store.getters.sideBarState).to.equal(true)
 
-    store.commit('app/sidebar/open', false)
-    expect(store.getters['app/sidebar/open']).to.equal(false)
+    store.commit('showSideBar', false)
+    expect(store.getters.sideBarState).to.equal(false)
   })
 
   it('sets and gets sidebar enabled state', () => {
-    store.commit('app/sidebar/enabled', true)
-    expect(store.getters['app/sidebar/enabled']).to.equal(true)
+    store.commit('sideBarEnabled', true)
+    expect(store.getters.sideBarEnabled).to.equal(true)
 
-    store.commit('app/sidebar/enabled', false)
-    expect(store.getters['app/sidebar/enabled']).to.equal(false)
+    store.commit('sideBarEnabled', false)
+    expect(store.getters.sideBarEnabled).to.equal(false)
   })
 
   it('sets and gets config', () => {
-    store.commit('app/config', false)
-    expect(store.getters['app/config']).to.equal(false)
+    store.commit('setConfig', false)
+    expect(store.getters.config).to.equal(false)
 
-    store.commit('app/config', true)
-    expect(store.getters['app/config']).to.equal(true)
+    store.commit('setConfig', true)
+    expect(store.getters.config).to.equal(true)
   })
 })
