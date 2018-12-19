5c8a6700c2d2d58865cd29333167aac7dfb1c180
Darryn Ten <darryn.ten@gmail.com>
2018-12-19 17:50:44 +0200

    Remove double user info get

--- src/api/user.js
+++ src/api/user.js
@@ -1,13 +1,4 @@
 import Vue from 'vue'
-import store from '@/store'
-
-export function loadUserDetails(userId) {
-  return Vue.axios.get('api/me')
-    .then(response => {
-      store.commit('setUser', response.data)
-      return response.data
-    })
-}
 
 export function resendVerification() {
   return Vue.axios.get(`/api/resend/verification`)
@@ -20,9 +11,4 @@ export async function logout() {
   await Vue.axios.post(`/logout`)
   await localStorage.clear()
   window.location.pathname = 'login'
-}
-
-export async function loadUserData(user) {
-  const userData = await loadUserDetails(user._id)
-  return userData
 }
