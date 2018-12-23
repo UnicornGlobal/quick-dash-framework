249f1675bc7b16359280b450b176615d602d66dc
Darryn Ten <darryn.ten@gmail.com>
2018-12-23 14:14:35 +0200

    Update admin configuration

--- src/router/admin.js
+++ src/router/admin.js
@@ -2,6 +2,38 @@ import Settings from '@/assets/icons/settings'
 import User from '@/assets/icons/user'
 import config from '@/config'
 
+const users = config.admin.users ? [
+  {
+    name: 'AllUsers',
+    path: 'users',
+    component: require('@/pages/Admin/Users/Index.vue').default,
+    meta: {
+      main: true,
+      label: 'All Users',
+      icon: User
+    }
+  },
+  {
+    name: 'UserDetails',
+    path: 'users/:userId',
+    component: require('@/pages/Admin/Users/UserDetails.vue').default,
+    props: true
+  }
+] : []
+
+const settings = config.admin.settings.enabled ? [
+  {
+    name: 'Settings',
+    path: 'settings',
+    component: config.admin.settings.component,
+    meta: {
+      main: true,
+      label: 'Settings',
+      icon: Settings
+    }
+  }
+] : []
+
 export default config.admin.enabled ? [
   {
     name: 'AdminAccount',
@@ -13,32 +45,8 @@ export default config.admin.enabled ? [
       icon: Settings
     },
     children: [
-      {
-        name: 'AllUsers',
-        path: 'users',
-        component: require('@/pages/Admin/Users/Index.vue').default,
-        meta: {
-          main: true,
-          label: 'All Users',
-          icon: User
-        }
-      },
-      {
-        name: 'UserDetails',
-        path: 'users/:userId',
-        component: require('@/pages/Admin/Users/UserDetails.vue').default,
-        props: true
-      },
-      {
-        name: 'Settings',
-        path: 'settings',
-        component: {template: '<router-view></router-view>'},
-        meta: {
-          main: true,
-          label: 'Settings',
-          icon: Settings
-        }
-      }
+      ...users,
+      ...settings
     ]
   }
 ] : []
