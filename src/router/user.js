3822cb54f9d663fc39e6d1e7067c41a31bc1c767
Darryn Ten <darryn.ten@gmail.com>
2018-12-23 13:53:52 +0200

    Configuration file mechanisms

--- src/router/user.js
+++ src/router/user.js
@@ -1,25 +1,31 @@
 import Home from '@/assets/icons/home'
 import User from '@/assets/icons/user'
+import config from '@/config'
+
+console.log(config)
+const home = config.router.home.enabled ? [{
+  name: 'Home',
+  path: '',
+  component: config.router.home.component,
+  meta: {
+    main: true,
+    label: 'Home',
+    icon: Home
+  }
+}] : []
+
+const account = config.router.account.enabled ? [{
+  name: 'Account',
+  path: '/account',
+  component: config.router.account.component,
+  meta: {
+    main: true,
+    label: 'Account',
+    icon: User
+  }
+}] : []
 
 export default [
-  {
-    name: 'Home',
-    path: '',
-    component: require('@/pages/User/Home/Home.vue').default,
-    meta: {
-      main: true,
-      label: 'Home',
-      icon: Home
-    }
-  },
-  {
-    name: 'Account',
-    path: '/account',
-    component: require('@/pages/User/Details.vue').default,
-    meta: {
-      main: true,
-      label: 'Account',
-      icon: User
-    }
-  }
+  ...home,
+  ...account
 ]
