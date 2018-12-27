39ec4fdaa4b5322fd64518dad91a515c5b242db3
Darryn Ten <darryn.ten@gmail.com>
2018-12-27 13:55:30 +0200

    Configurable header

--- src/components/TopNav/TopNav.vue
+++ src/components/TopNav/TopNav.vue
@@ -1,33 +1,15 @@
 <template>
-  <div>
-    <div class="top-navigation">
-      <div class="header" :style="headerStyle">
-        <div class="menu-toggle">
-          <div @click.prevent="toggleSideBar()">
-            <hamburger></hamburger>
-          </div>
-        </div>
-        <router-link class="logo" to="/">
-          Quick Dash
-        </router-link>
-        <div v-if="user" class="user-menu">
-          <avatar-or-initials
-            class="user-avatar"
-            :size="40"
-            :title="user.first_name"
-            backgroundColour="#4CAF50"
-          >
-          </avatar-or-initials>
-          <div class="user-details">
-            <div class="user-name text-white">
-              {{ user.first_name}} {{ user.last_name }}
-            </div>
-            <div class="user-company text-white">
-              <a @click.prevent="logout" href="#" class="logout-button">Logout</a>
-            </div>
-          </div>
+  <div class="top-navigation">
+    <div class="header" :style="headerStyle">
+      <div class="menu-toggle">
+        <div @click.prevent="toggleSideBar()">
+          <hamburger></hamburger>
         </div>
       </div>
+      <router-link class="logo" to="/">
+        Quick Dash
+      </router-link>
+      <user-menu v-if="user" :user="user"></user-menu>
     </div>
   </div>
 </template>
@@ -92,87 +74,17 @@
       fill: $white;
     }
   }
-
-  .user-menu {
-    // background: $accent;
-    // width: 350px;
-    height: 60px;
-  }
-
-  .fill {
-    flex: 1;
-  }
-
-  .user-menu {
-    display: flex;
-
-    a {
-      color: $white;
-    }
-
-    @media (max-width: 1024px) {
-      display: none;
-    }
-  }
-
-  .user-avatar {
-    width: 65px;
-    height: 60px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-
-    .avatar-initials {
-      background-color: $accent;
-      border: 2px solid $white;
-    }
-  }
-
-  .user-details {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    flex: 1;
-    color: $white;
-
-    .user-name {
-      font-weight: bold;
-      white-space: nowrap;
-      overflow: hidden;
-      text-overflow: ellipsis;
-    }
-
-    .user-company {
-      font-size: 12px;
-    }
-  }
-
-  .user-menu-dropdown-toggle {
-    width: 64px;
-    height: 64px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    color: $white;
-    font-size: 24px;
-    font-weight: bold;
-    svg {
-      width: 30px;
-      height: 30px;
-      fill: $white;
-    }
-  }
 </style>
 
 <script>
-  import AvatarOrInitials from '@unicorns/avatars'
+  import UserMenu from '@/components/TopNav/UserMenu'
   import { reloadRouter } from '@/router'
   import icons from '@/icons'
 
   export default {
     name: 'top-nav',
     components: {
-      AvatarOrInitials,
+      UserMenu,
       hamburger: icons.hamburger
     },
     props: {
