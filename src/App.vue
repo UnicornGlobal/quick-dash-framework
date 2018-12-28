373c38c338ce7f688d1b6d21e50d90645cfb3433
Darryn Ten <darryn.ten@gmail.com>
2018-12-28 14:36:12 +0200

    Mobile navigation end-to-end tests

--- src/App.vue
+++ src/App.vue
@@ -172,11 +172,14 @@
   }
 
   .shadow {
+    display: block;
     position: fixed;
+    width: 100%;
+    height: 100%;
     top: 0;
-    left: 0;
     right: 0;
     bottom: 0;
+    left: 0;
     z-index: 1;
     background-color: rgba(0, 0, 0, 0.6);
   }
