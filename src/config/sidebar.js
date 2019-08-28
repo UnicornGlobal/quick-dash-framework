f8ecc617f40e0ed0a699d435554d674fd5aeb40e
Darryn Ten <darryn.ten@gmail.com>
2019-08-28 16:33:32 +0700

    Use default icon

--- src/config/sidebar.js
+++ src/config/sidebar.js
@@ -1,9 +1,13 @@
+import icons from '@/icons'
+
 export default {
   enabled: true,
   open: true,
   position: 'left',
   logout: true,
-  logo: true,
+  logo: () => {
+    return icons.quick
+  },
   profile: true,
   highlight: true,
   icons: true
