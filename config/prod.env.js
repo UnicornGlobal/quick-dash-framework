d7b250afa9bd4339119dbf9fd48e94026e95ab19
Restyled.io <commits@restyled.io>
2019-09-13 07:07:24 +0000

    Restyled by prettier

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,7 +1,7 @@
-'use strict'
-const merge = require('webpack-merge')
-const secrets = require('./secrets.env.js.example')
+"use strict";
+const merge = require("webpack-merge");
+const secrets = require("./secrets.env.js.example");
 
 module.exports = merge(secrets, {
   NODE_ENV: '"production"'
-})
+});
