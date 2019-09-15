a05922adc89d9d47776b835c6166298faa4d74ef
Darryn Ten <darryn.ten@gmail.com>
2019-09-15 10:41:11 +0800

    Debugging travis build

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,6 +1,6 @@
 'use strict'
 const merge = require('webpack-merge')
-const secrets = require('./secrets.env.js.example')
+const secrets = require('./secrets.env.example.js')
 
 module.exports = merge(secrets, {
   NODE_ENV: '"production"'
