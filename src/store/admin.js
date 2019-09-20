34c3e4ace8ea2d9b8c25d80c3de8655b80506156
Darryn Ten <darryn.ten@gmail.com>
2019-09-20 15:25:08 +0800

    Merge admin stores

--- src/store/admin.js
+++ src/store/admin.js
@@ -1,7 +1,11 @@
 import users from '@/store/users'
 import roles from '@/store/roles'
 
-export default {
+let custom = false
+let external = {}
+
+// The framework level store
+let adminStore = {
   namespaced: true,
   state: {
     ...users.state,
@@ -16,3 +20,41 @@ export default {
     ...roles.getters
   }
 }
+
+// Load any app level admin only stores
+try {
+  custom = require.context('~/store/admin', true, /\.js$/)
+  custom.keys().forEach(function(key) {
+    const name = /\.\/(\S+)\.js/.exec(key)[1]
+
+    console.log('Found custom admin only store: ', name)
+
+    external = {
+      ...external,
+      [name]: custom(key).default
+    }
+  })
+} catch (e) {
+  console.log('Loading custom admin store error:', e)
+}
+
+// Load the new custom stores into the namespace
+if (external) {
+  console.log([external])
+  Object.keys(external).forEach(function(key) {
+    adminStore.state = {
+      ...adminStore.state,
+      ...external[key].state
+    }
+    adminStore.mutations = {
+      ...adminStore.mutations,
+      ...external[key].mutations
+    }
+    adminStore.getters = {
+      ...adminStore.getters,
+      ...external[key].getters
+    }
+  })
+}
+
+export default adminStore
