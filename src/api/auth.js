8259fd64e86fcfcf718fc7e3a890e72b8b56ab7e
Darryn Ten <darryn.ten@gmail.com>
2020-05-12 16:58:48 +0700

    Clear cookies on logout

--- src/api/auth.js
+++ src/api/auth.js
@@ -3,6 +3,10 @@ import Vue from 'vue'
 export async function logout() {
   await Vue.axios.post(`/logout`)
   await localStorage.clear()
+  // Clear cookies too
+  document.cookie.split(';').forEach(function(c) {
+    document.cookie = c.trim().split('=')[0] + '=;' + 'expires=Thu, 01 Jan 1970 00:00:00 UTC;'
+  })
   window.location.href = '/login?logout=true'
 }
 
